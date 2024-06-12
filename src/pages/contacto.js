c82ba536413083f6e166d44a5e06f542a9c13914
AElenaGr <andrae.tandemaranjuez@gmail.com>
2024-06-12 12:09:24 +0200

    sddasd

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,16 +1,20 @@
-// src/components/ContactDetails.js
-
-import React from "react"
+import React from "react";
+import Layout from '../components/layout';
+import '../components/layout.css';
 
 const ContactDetails = () => {
   return (
-    <div className="contact-details">
-      <h2>Contacto de Patrimonio Nacional</h2>
-      <p>Dirección: Calle de la Reina, 1, 28001 Madrid, España</p>
-      <p>Teléfono: +34 91 454 88 00</p>
-      <p>Email: info@patrimonionacional.es</p>
-    </div>
-  )
+    <Layout>
+      <div className="container">
+        <div className="contact-details">
+          <h2>Contacto de Patrimonio Nacional</h2>
+          <p>Dirección: Calle de la Reina, 1, 28001 Madrid, España</p>
+          <p>Teléfono: +34 91 454 88 00</p>
+          <p>Email: info@patrimonionacional.es</p>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
-export default ContactDetails
+export default ContactDetails;
