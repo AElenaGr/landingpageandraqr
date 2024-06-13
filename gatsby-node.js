e15c54065bc6e800c6f4c845f65fec48ad28b251
AElenaGr <andrae.tandemaranjuez@gmail.com>
2024-06-13 08:34:08 +0200

    13 junio 1

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,3 +16,18 @@ exports.createPages = async ({ actions }) => {
     defer: true,
   })
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-leaflet|leaflet/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
