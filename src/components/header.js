f3544666021182907ebbb99b320a9860aa2e62c7
AElenaGr <andrae.tandemaranjuez@gmail.com>
2024-07-05 12:14:52 +0200

    5 julio 2024 lista segun rol

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,30 @@
-import * as React from "react";
-import { Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
-import logoutIcon from "../images/logout-icon.png";
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+// Importa el ícono de logout
+import logoutIcon from "../images/logout-icon.png"
 
 const Header = () => {
-  const [tandemNombre, setTandemNombre] = React.useState('');
-  const [tandemRole, setTandemRole] = React.useState('');
+  const [tandemNombre, setTandemNombre] = React.useState('')
+  const [role, setRole] = React.useState('')
 
   React.useEffect(() => {
     if (typeof window !== 'undefined') {
+      // Accede a localStorage solo en el entorno del navegador
       const nombre = localStorage.getItem('tandem_nombre');
-      const role = localStorage.getItem('tandem_role');
+      const storedRole = localStorage.getItem('tandem_role');
       setTandemNombre(nombre);
-      setTandemRole(role);
+      setRole(storedRole);
     }
-  }, []);
+  }, [])
 
+  // Función para manejar el logout
   const handleLogout = () => {
+    // Elimina el usuario del almacenamiento local
     localStorage.removeItem("tandem_nombre");
     localStorage.removeItem("tandem_role");
-    localStorage.removeItem("tandem_email");
-    localStorage.removeItem("tandem_id");
-    localStorage.removeItem("loggedInUser");
+    // Redirige a la página de inicio
     window.location.replace("/");
   };
 
@@ -51,39 +54,99 @@ const Header = () => {
       </Link>
 
       <nav style={{ display: "flex", alignItems: "center" }}>
-        <Link to="/" style={{ margin: "0 1rem", textDecoration: "none", color: "black" }}>Inicio</Link>
+        <Link
+          to="/"
+          style={{ margin: "0 1rem", textDecoration: "none", color: "black" }}
+        >
+          Inicio
+        </Link>
         <span style={{ margin: "0 0.5rem" }}>|</span>
-        <Link to="/formulario" style={{ margin: "0 1rem", textDecoration: "none", color: "black" }}>Acceso Empleados</Link>
+        <Link
+          to="/formulario"
+          style={{ margin: "0 1rem", textDecoration: "none", color: "black" }}
+        >
+          Acceso Empleados
+        </Link>
+        {role === 'admin' && (
+          <>
+            <span style={{ margin: "0 0.5rem" }}>|</span>
+            <Link
+              to="http://localhost:8000/admin-user/"
+              style={{
+                margin: "0 1rem",
+                textDecoration: "none",
+                color: "black",
+              }}
+            >
+              Ver Usuarios
+            </Link>
+          </>
+        )}
         {tandemNombre && (
           <>
             <span style={{ margin: "0 0.5rem" }}>|</span>
-            <Link to="/page-2" style={{ margin: "0 1rem", textDecoration: "none", color: "black" }}>Generador de QR</Link>
+            <Link
+              to="/page-2"
+              style={{
+                margin: "0 1rem",
+                textDecoration: "none",
+                color: "black",
+              }}
+            >
+              Generador de QR
+            </Link>
             <span style={{ margin: "0 0.5rem" }}>|</span>
-            <Link to="/profile" style={{ margin: "0 1rem", textDecoration: "none", color: "black" }}>Perfil</Link>
-          </>
-        )}
-        <span style={{ margin: "0 0.5rem" }}>|</span>
-        <Link to="/page-3" style={{ margin: "0 1rem", textDecoration: "none", color: "black" }}>Información Institucional</Link>
-        {tandemRole === 'admin' && (
-          <>
+            <Link
+              to="/profile"
+              style={{
+                margin: "0 1rem",
+                textDecoration: "none",
+                color: "black",
+              }}
+            >
+              Perfil
+            </Link>
             <span style={{ margin: "0 0.5rem" }}>|</span>
-            <Link to="http://localhost:8000/admin-user/" style={{ margin: "0 1rem", textDecoration: "none", color: "black" }}>Ver Usuarios</Link>
+            <Link
+              to="/page-3"
+              style={{
+                margin: "0 1rem",
+                textDecoration: "none",
+                color: "black",
+              }}
+            >
+              Información Institucional
+            </Link>
           </>
         )}
       </nav>
 
       {tandemNombre ? (
         <div style={{ display: "flex", alignItems: "center" }}>
-          <p style={{ marginRight: "1rem" }}>¡Bienvenido, {tandemNombre}!</p>
-          <button onClick={handleLogout} style={{ border: "none", background: "none", cursor: "pointer" }}>
-            <img src={logoutIcon} alt="Logout" style={{ width: "24px", height: "24px" }} />
+          <p style={{ marginRight: "1rem" }}>
+            ¡Bienvenido, {tandemNombre}!
+          </p>
+          <button
+            onClick={handleLogout}
+            style={{ border: "none", background: "none", cursor: "pointer" }}
+          >
+            <img
+              src={logoutIcon}
+              alt="Logout"
+              style={{ width: "24px", height: "24px" }}
+            />
           </button>
         </div>
       ) : (
-        <Link to="/formulario" style={{ marginLeft: "auto", textDecoration: "none", color: "black" }}>Iniciar Sesión</Link>
+        <Link
+          to="/formulario"
+          style={{ marginLeft: "auto", textDecoration: "none", color: "black" }}
+        >
+          Iniciar Sesión
+        </Link>
       )}
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
