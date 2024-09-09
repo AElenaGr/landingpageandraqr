edb1c0675610ce7ff5ca9e9d55355c245b82f7ce
AElenaGr <andrae.tandemaranjuez@gmail.com>
2024-09-09 09:30:45 +0200

    savedb actualizada

--- src/components/savedb.js
+++ src/components/savedb.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from "@mui/material";
 
-const SaveDb= ({data,nref,desc})=>{
-const [mensaje, setMensaje]=useState(false)
-const [respuesta, setRespuesta]=useState('')
-const userId = localStorage.getItem('tandem_id')
+const SaveDb = ({data, nref, desc}) => {
+    const [mensaje, setMensaje] = useState(false);
+    const [respuesta, setRespuesta] = useState('');
+    const [userId, setUserId] = useState(null);
 
-    const saveQr= async () => {
-    //    http://andra.tandempatrimonionacional.eu/api-qr-tandem/v1/create-qr.php
-    //    http://localhost/api-qr-tandem/v1/create-qr.php
+    // Solo accede a localStorage cuando el componente está montado en el cliente
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const storedUserId = localStorage.getItem('tandem_id');
+            setUserId(storedUserId);
+        }
+    }, []);
+
+    const saveQr = async () => {
         try {
             const response = await fetch('https://andra.tandempatrimonionacional.eu/api-qr-tandem/v1/create-qr.php', {
                 method: 'POST',
@@ -22,24 +28,21 @@ const userId = localStorage.getItem('tandem_id')
                     "created_by": userId
                 })
             });
-            const respuesta=await response.json();
-           setRespuesta(respuesta.message)
-           setMensaje(true)
-           console.log(respuesta.message)
-
+            const respuesta = await response.json();
+            setRespuesta(respuesta.message);
+            setMensaje(true);
+            console.log(respuesta.message);
         } catch (error) {
             console.error('Error creando código QR', error);
-        
         }
     };
 
-   return(
-    <>
-    <Button onClick={saveQr} className='button-collapse'>Guardar</Button>
-    {mensaje && <p>{respuesta.message}</p> }
-    </>
-   ) 
-}
+    return (
+        <>
+            <Button onClick={saveQr} className='button-collapse'>Guardar</Button>
+            {mensaje && <p>{respuesta}</p>}
+        </>
+    );
+};
 
-
-export default SaveDb
+export default SaveDb;
